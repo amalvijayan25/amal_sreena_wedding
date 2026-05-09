d96a8511852dcd511e394c529c24ca7e19cb1faa
Amal Vijayan <amal.vijayan2506@gmail.com>
2026-05-09 10:44:01 +0000

    Fix countdown timer to update in real-time every second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Amal & Sreena';
   googleDriveUploadUrl = 'https://drive.google.com/drive/folders/YOUR_GOOGLE_DRIVE_FOLDER_ID?usp=sharing';
   weddingMapUrl = 'https://maps.app.goo.gl/CzY9825VpdgCGL8A7';
   receptionMapUrl = 'https://maps.app.goo.gl/XPcDztG8t2naX65r8';
   countdownDays: number;
   countdownHours: number;
+  private countdownInterval: any;
 
   wedding = {
     date: 'Friday, 28 August 2026',
@@ -29,11 +30,23 @@ export class AppComponent {
     this.calculateCountdown();
   }
 
+  ngOnInit() {
+    this.countdownInterval = setInterval(() => {
+      this.calculateCountdown();
+    }, 1000);
+  }
+
   calculateCountdown() {
     const weddingDate = new Date('2026-08-28T12:25:00');
     const now = new Date();
     const diffMs = weddingDate.getTime() - now.getTime();
     this.countdownDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
     this.countdownHours = Math.floor((diffMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  }
+
+  ngOnDestroy() {
+    if (this.countdownInterval) {
+      clearInterval(this.countdownInterval);
+    }
   }
 }
