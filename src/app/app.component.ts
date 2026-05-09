ca0302bc2ad316f6250fee6ca409865b9e324716
Amal Vijayan <amal.vijayan2506@gmail.com>
2026-05-09 09:17:51 +0000

    Add venue location buttons for wedding ceremony and reception

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Amal & Sreena';
   googleDriveUploadUrl = 'https://drive.google.com/drive/folders/YOUR_GOOGLE_DRIVE_FOLDER_ID?usp=sharing';
+  weddingMapUrl = 'https://maps.app.goo.gl/CzY9825VpdgCGL8A7';
+  receptionMapUrl = 'https://maps.app.goo.gl/XPcDztG8t2naX65r8';
   wedding = {
-    date: 'Saturday, 20 July 2026',
+    date: 'Friday, 28 August 2026',
     time: '5:00 PM',
     location: 'Blue Lotus Garden, Chennai'
   };
   reception = {
-    date: 'Sunday, 21 July 2026',
+    date: 'Saturday, 29 August 2026',
     time: '7:00 PM',
     location: 'Neon Ballroom, Starview Convention Center, Chennai'
   };
