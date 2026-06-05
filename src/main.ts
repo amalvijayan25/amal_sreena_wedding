33ae288d1a2983ad05fdea8a297c8430bcc9de8d
Amal Vijayan <amal.vijayan2506@gmail.com>
2026-06-05 14:41:52 +0530

    angular version updated and code changed to v22

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-bootstrapApplication(AppComponent, {providers: [provideZoneChangeDetection()]}).catch(err => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
